04676359563894a97fc46c7c89d3fb52b33b6856
eduardobosco <eduardoboscocs@gmail.com>
2020-09-23 11:14:05 -0300

    layout

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,12 +1,15 @@
 import React from 'react';
 import '../Home/styles.css'
 import { ReactComponent as DashImage } from '../../Assets/dashImage.svg'
+import { ReactComponent as Logo } from '../../Assets/logo.svg'
+
 
 const Home = () => (
 
   <div className="home-container">
         <div className="home-text">
-          <h1 className="home-text-title">Bem Vindo</h1>
+          <Logo className="home-logo"/>
+          <h1 className="home-text-title">Bem Vindo!</h1>
           <h3 className="home-text-subtitle">Painel Administrativo Pencet</h3>
         </div>
       <DashImage className='home-image' />
